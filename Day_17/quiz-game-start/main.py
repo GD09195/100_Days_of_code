3f2b2dc1f23f18e7e9038a4e806ece0c35037840
gd091295 <gd091295@gmail.com>
2024-07-23 22:08:31 -0600

    Finishing Day 17.

--- Day_17/quiz-game-start/main.py
+++ Day_17/quiz-game-start/main.py
@@ -2,7 +2,12 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank: list = [Question(question['text'], question['answer']) for question in question_data]
+question_bank: list = [Question(question['question'], question['correct_answer']) for question in question_data]
 quizz_logic = QuizBrain(question_bank)
 
-quizz_logic.next_question()
+while quizz_logic.still_has_questions():
+
+    quizz_logic.next_question()
+
+print("You've complete the quizz")
+print(f"Your final score is {quizz_logic.score} / {quizz_logic.question_number}")
--- Day_17/quiz-game-start/quiz_brain.py
+++ Day_17/quiz-game-start/quiz_brain.py
@@ -2,17 +2,26 @@ class QuizBrain:
     def __init__(self, new_question_list):
         self.question_number = 0
         self.question_list = new_question_list
+        self.score = 0
 
     def next_question(self):
 
         following_question = self.question_list[self.question_number]
         self.question_number += 1
         answer = input(f"Q.{self.question_number}: {following_question.question} (True/False): ").lower()
+        self.check_answer(answer, following_question.answer)
 
-        if answer == following_question.answer.lower():
+    def still_has_questions(self) -> bool:
+        return self.question_number != len(self.question_list)
+
+    def check_answer(self, user_answer, question_answer):
+        if user_answer.lower() == question_answer.lower():
+            self.score += 1
             print("Correct answer")
         else:
-            print("incorrect answer")
+            print("Incorrect answer")
+
+        print(f"Your current score is {self.score} / {self.question_number}\n")
 
 
 
